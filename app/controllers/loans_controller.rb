c47728d5855487941682b9367b3479f324451f43
hortense gilbert <hortensegilbert@hotmail.com>
2017-12-26 11:34:39 +0100

    working adding loan

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -3,23 +3,27 @@ class LoansController < ApplicationController
 
   def new
     @loan = Loan.new
-    @user = current_user
   end
 
   def create
-    # @loan = Loan.new(loan_params)
-    # # @loan.user = @user
-    # @loan.save
+    @loan = Loan.new(loan_params)
+    @loan.user = @user
+    if @loan.save
+      redirect_to loans_path
+    else
+      render :new
+    end
   end
 
 
   private
+
   def set_user
     @user = current_user
   end
   def loan_params
+    params[:principal] * 100 if !params[:principal].nil?
     params.require(:loan).permit(:lender, :borrower, :principal, :termLength, :annualRate, :typeLoan, :frequencyPayment, :firstPaymentDate, :StartingMonthDelayed)
   end
-
 
 end
--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,4 +1,17 @@
 class Loan < ApplicationRecord
   belongs_to :schedule, dependent: :destroy
   belongs_to :user
+
+  validates :lender, presence: true
+  validates :borrower, presence: true
+  validates :principal, presence: true
+  validates :termLength, presence: true,  numericality: true
+  validates :annualRate, presence: true, numericality: true
+  validates :typeLoan, presence: true
+  validates :frequencyPayment, presence: true
+  validates :firstPaymentDate, presence: true
+  validates :StartingMonthDelayed, numericality: true
+
 end
+
+
