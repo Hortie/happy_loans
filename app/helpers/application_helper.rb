41b20938fe9348e1badc78cfd931a7bcd1e60003
hortense gilbert <hortensegilbert@hotmail.com>
2018-01-05 13:36:25 +0100

    pas trop mal. 1er graphique statique

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+def loans_by_borrower
+  bar_chart @loans.group(:borrower).count, height: '500px'
+end
+
+def initial_capital_by_borrower
+  column_chart initial_capital_by_borrower_charts_path, stacked: true
+end
+
 end
