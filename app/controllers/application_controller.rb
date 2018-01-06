b14adca58f8f3534f74607a7929e3ffb84f8308c
hortense gilbert <hortensegilbert@hotmail.com>
2018-01-06 17:05:40 +0100

    ok for graphs static

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
+#  save ou display  aux cents ou mills
   def get_amout(number)
     return number.to_f / 100
   end
@@ -24,5 +25,14 @@ class ApplicationController < ActionController::Base
     return (number * 100 ).to_i
   end
 
-  helper_method :get_amout, :get_rate, :display_amount, :display_rate, :display_date, :save_amount, :save_rate
+helper_method :get_amout, :get_rate, :display_amount, :display_rate, :display_date, :save_amount, :save_rate
+
+# get data
+
+  def get_loans_id
+   return Loan.where(user_id: current_user).map { |x| x.id }
+  end
+
+helper_method :get_loans_id
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,28 @@
 module ApplicationHelper
 
+# capital restant du, last due_date
+def capital_restant_du(loan, ddate)
+  ss = Schedule.where(loan_id: loan.id, tokeep: true).where("due_date > ?", ddate).order(:due_date).first
+  return display_amount(ss.principal_balance)
+end
+
+def capital_restant_du_total(ddate)
+  loan_ids = get_loans_id
+  sum= 0
+  loan_ids.each do |id|
+    ss = Schedule.where(loan_id: id, tokeep: true).where("due_date > ?", ddate).order(:due_date).first
+    sum +=  ss.principal_balance
+  end
+  return display_amount(sum)
+end
+
+def last_due_date(loan, ddate)
+  ss = Schedule.where(loan_id: loan.id, tokeep: true).order(:due_date).last
+  return display_date(ss.due_date)
+end
+
+
+# ********CHARTS ****************
 def loans_by_borrower
   bar_chart @loans.group(:borrower).count, height: '500px'
 end
@@ -9,7 +32,9 @@ def initial_capital_by_borrower
 end
 
 def due_per_period
-  column_chart due_per_period_charts_path, stacked: true
+  column_chart due_per_period_charts_path, stacked: true,  legend: false,  download: true, height: '500px', width: '800px', xtitle: "Années", ytitle: "Capital due"
 end
+
+
 
 end
